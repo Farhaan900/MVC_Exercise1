8d853d09938ebd7368b75ce1357a91eb2a667d66
Farhaan900 <mfarhaan.shaikh95@gmail.com>
2019-03-06 10:14:05 +0530

    commit 2

--- src/main/java/com/stackroute/UserController.java
+++ src/main/java/com/stackroute/UserController.java
@@ -7,6 +7,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+/**
+ *  Create a annotation based Spring MVC application which has following classes:
+ *  UserController and user class.
+ *  UserController will create user object and will send user object to View Page (index) .
+ *  View Page Should display “Welcome <user> to stackroute”.
+ */
+
 @RequestMapping("")
 @Controller
 public class UserController {
